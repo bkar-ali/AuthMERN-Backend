975d064b54626197dfeda86799ff0398043c3162
Abubakr <bkarali345@gmail.com>
2026-07-02 00:40:28 +0300

    debuging

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import { env } from "./config/env.js";
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies (req.body)
 
+console.log("CLIENT_URL =", env.CLIENT_URL);
+
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: env.CLIENT_URL, // Allow requests from this origin
     credentials: true, // Allow cookies to be sent with requests
   }),
 );
